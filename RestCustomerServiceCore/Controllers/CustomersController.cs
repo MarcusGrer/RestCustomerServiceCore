0f65ba960410d9eb1165543c0a70b90f74e222fc
Marek Laslo <mare1168@edu.easj.dk>
2018-10-24 22:27:14 +0200

    safsdf

---
+++ RestCustomerServiceCore/Controllers/CustomersController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RestCustomerServiceCore.Model;
+
+namespace RestCustomerServiceCore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+
+        private static List<Customer> cList = new List<Customer>
+        {
+            new Customer(){FirstName = "Kokot", Id = 1,LastName = "Demovic", YearOfRegistration = 15112018},
+            new Customer(){FirstName = "Robot", Id = 2,LastName = "Genovic", YearOfRegistration = 24051235},
+            new Customer(){FirstName = "Chobot", Id = 3,LastName = "Lenovic", YearOfRegistration = 44531532},
+        };
+
+        
+
+        // GET: api/Customers
+        [HttpGet]
+        public IEnumerable<Customer> Get()
+        {
+            return cList;
+        }
+
+        // GET: api/Customers/5
+        [HttpGet("{id}", Name = "Get")]
+        public Customer Get(int id)
+        {
+            return cList[id-1];
+        }
+
+        // POST: api/Customers
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Customers/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(Customer id)
+        {
+            cList.Remove(id);
+        }
+    }
+}
---
+++ RestCustomerServiceCore/Model/Customer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestCustomerServiceCore.Model
+{
+    public class Customer
+    {
+        private int _id;
+        private string _firstName;
+        private string _lastName;
+        private int _yearOfRegistration;
+
+        public int Id { get => _id; set => _id = value; }
+        public string FirstName { get => _firstName; set => _firstName = value; }
+        public string LastName { get => _lastName; set => _lastName = value; }
+        public int YearOfRegistration { get => _yearOfRegistration; set => _yearOfRegistration = value; }
+
+        public Customer(int id, string firstName, string lastName, int yearOfRegistration)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            YearOfRegistration = yearOfRegistration;
+        }
+
+        public Customer()
+        {
+
+        }
+    }
+}
